48bcb92ffb5a38b77c0d31f01921ae720ff60676
Harshil2107 <harshilp2107@gmail.com>
2022-04-16 15:17:38 -0700

    login using dicord - redirection done

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -2,12 +2,10 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../styles/AddClass.css";
 import { Form, FormControl, Button } from "react-bootstrap";
-
 function Login() {
   let disc_redirect = () => {
-    console.log(process.env.REACT_APP_CLIENT_ID);
-    console.log(process.env.REACT_APP_CLIENT_SECRET);
-    console.log(1);
+    
+    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=964993808888643614&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=code&scope=identify";
   };
   return (
     <div className="add-login">
