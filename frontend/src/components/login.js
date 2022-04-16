c6176c3f9da7cc8490177929374b58064779ea38
Harshil2107 <harshilp2107@gmail.com>
2022-04-16 14:43:13 -0700

    login added error env

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -4,7 +4,9 @@ import "../styles/AddClass.css";
 import { Form, FormControl, Button } from "react-bootstrap";
 function Login() {
     let disc_redirect = () => {
-
+        console.log(process.env.REACT_APP_CLIENT_ID)   
+        console.log(process.env.REACT_APP_CLIENT_SECRET)
+        console.log(1)
     };
     return (
         <div className="add-login">
