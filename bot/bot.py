c9d4d6d607a54b62dd87c04356680faaaacd714a
kunpai <kunpai@ucdavis.edu>
2022-04-16 12:45:06 -0700

    inital bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,3 +1,4 @@
+from aiohttp import request
 import discord
 import os
 import random
@@ -5,6 +6,7 @@ import interactions
 from discord_slash import SlashCommand, SlashContext
 from discord.ext import tasks
 from dotenv import load_dotenv
+import requests
 load_dotenv()
 
 intents = discord.Intents.default()
@@ -29,5 +31,13 @@ async def _faq(ctx= SlashContext, *, link = None):
     url = url.replace(' ', '-')
     url = "https://gatosecksual.kunpai.space/" + url
     await ctx.send(url)
+
+@slash.slash(name="course", description="View information for a course")
+async def _course(ctx = SlashContext, *, course = str, section: str = None):
+    link = "http://127.0.0.1:5000/course/" + course
+    retjson = requests.get(url=link)
+    print(retjson.dump)
+    
+
 
 client.run(os.environ['TOKEN'])
