0de8fecb0044aa6153db2a477ddbba12263ced66
Parth Shah <helloparthshah@gmail.com>
2023-01-13 13:39:50 -0800

    gh pages deploy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,9 +2,15 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "http://localhost:5000",
+    createProxyMiddleware("/api", {
+      target: "https://unify.onrender.com/",
+      changeOrigin: true,
     })
   );
+  /* app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "https://unify.onrender.com/",
+    })
+  ); */
 };
